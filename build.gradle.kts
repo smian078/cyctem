4295a7b994a37665672bf5020d827711ccd3531a
smian078 <amankawale007@gmail.com>
2026-09-20 03:12:29 +0530

    build: align AGP with current AndroidX requirements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "9.0.1" apply false
+    id("com.android.application") version "9.1.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }

a8b2ce729c8d35efb0c7424255fdb4ad61de0cd4
smian078 <amankawale007@gmail.com>
2026-09-20 10:06:33 +0530

    test: add main activity launch smoke test

---
+++ app/src/androidTest/java/com/cystem/MainActivityLaunchTest.kt
@@ -0,0 +1,18 @@
+package com.cystem
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityLaunchTest {
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun mainActivityLaunchesWithoutCrash() {
+        composeRule.waitForIdle()
+    }
+}

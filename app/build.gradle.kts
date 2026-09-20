c62cbddd2c15a9a361c7225b2eb1b5dd68a77936
smian078 <amankawale007@gmail.com>
2026-09-20 10:07:35 +0530

    fix: disable release shrinking until startup smoke test is clean

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
